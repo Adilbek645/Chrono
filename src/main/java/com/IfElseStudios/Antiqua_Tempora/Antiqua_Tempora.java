56f6bf41ede57bddf57a1ebfed32b24cf46ec5aa
Adilbek645 <baber.channel.3@gmail.com>
2026-06-25 22:20:20 +0500

    Настройка

---
+++ src/main/java/com/IfElseStudios/Antiqua_Tempora/Antiqua_Tempora.java
@@ -0,0 +1,16 @@
+package com.IfElseStudios.Antiqua_Tempora;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(Antiqua_Tempora.MODID)
+public class Antiqua_Tempora
+{
+    public static final String MODID = "antiqua_tempora_mod";
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
+
+}
